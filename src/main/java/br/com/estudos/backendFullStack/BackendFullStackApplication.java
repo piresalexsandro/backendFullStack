b88110683b9c519436837f9c15e4e12206724df5
piresalexsandro <allexsandro.pires@gmail.com>
2021-04-19 23:17:52 -0300

    First commit

---
+++ src/main/java/br/com/estudos/backendFullStack/BackendFullStackApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.backendFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFullStackApplication.class, args);
+	}
+
+}
