f9acd1ac28860057d7948748ade587827c4d4486
piresalexsandro <allexsandro.pires@gmail.com>
2021-04-20 00:34:15 -0300

    15. Testando a primeira classe de domínio - Categoria

--- src/main/java/br/com/estudos/backendFullStack/resources/CategoriaResource.java
+++ src/main/java/br/com/estudos/backendFullStack/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.estudos.backendFullStack.resources;
 
+import br.com.estudos.backendFullStack.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "Rest esta funcionando!!";
+    public List<Categoria>  listar(){
+
+        Categoria c1 = new Categoria(1, "Software");
+        Categoria c2 = new Categoria(2, "Artificial Intelligence");
+
+        List<Categoria> list = new ArrayList<>();
+
+        list.add(c1);
+        list.add(c2);
+
+        return list;
     }
 
 }
